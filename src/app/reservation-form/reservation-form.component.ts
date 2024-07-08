441ccfe8eadefa582fa0f0e111bae70d0a9927e6
akinpinkman <akinbilgin54@hotmail.com>
2024-07-08 20:45:11 +0300

    Update reservation form component to use observables for API calls

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -31,9 +31,11 @@ export class ReservationFormComponent implements OnInit {
     let id = this.activatedRoute.snapshot.paramMap.get('id');
 
     if (id) {
-      let reservation = this.reservationService.getReservation(id);
-
-      if (reservation) this.reservationForm.patchValue(reservation);
+      let reservation = this.reservationService
+        .getReservation(id)
+        ?.subscribe((reservation) => {
+          if (reservation) this.reservationForm.patchValue(reservation);
+        });
     }
   }
 
@@ -44,9 +46,15 @@ export class ReservationFormComponent implements OnInit {
       let id = this.activatedRoute.snapshot.paramMap.get('id');
 
       if (id) {
-        this.reservationService.updateReservation(id, reservation);
+        this.reservationService
+          .updateReservation(id, reservation)
+          .subscribe(() => {
+            console.log('Update işlemde');
+          });
       } else {
-        this.reservationService.addReservation(reservation);
+        this.reservationService.addReservation(reservation).subscribe(() => {
+          console.log('Ekleme işlemde');
+        });
       }
 
       this.router.navigate(['/list']);
