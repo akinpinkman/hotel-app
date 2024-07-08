29e5b1d4326fbaee2f3edabb7bfc681d03bd6c1c
akinpinkman <akinbilgin54@hotmail.com>
2024-07-08 15:41:14 +0300

    Refactor ReservationService to remove local storage usage

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -7,11 +7,6 @@ import { Reservation } from '../models/reservation';
 export class ReservationService {
   private reservations: Reservation[] = [];
 
-  constructor() {
-    let savedReservations = localStorage.getItem('reservations');
-    this.reservations = savedReservations ? JSON.parse(savedReservations) : [];
-  }
-
   getReservations(): Reservation[] {
     return this.reservations;
   }
@@ -24,18 +19,15 @@ export class ReservationService {
     reservation.id = Date.now().toString();
 
     this.reservations.push(reservation);
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   deleteReservation(id: string): void {
     let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations.splice(index, 1);
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 
   updateReservation(id: string, updatedReservation: Reservation): void {
     let index = this.reservations.findIndex((res) => res.id === id);
     this.reservations[index] = updatedReservation;
-    localStorage.setItem('reservations', JSON.stringify(this.reservations));
   }
 }
